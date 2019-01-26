f54adf8d42f5f851e6778460a66e3705abbced0c
Alan Chambers <ajmchambers@users.noreply.github.com>
2019-01-26 11:52:31 +0000

    added inputs

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 @Component({
@@ -7,26 +7,56 @@ import { format } from '../../utils/utils';
   shadow: true
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
+  @Prop({ mutable: true, reflectToAttr: true }) first: string;
+  @Prop({ mutable: true, reflectToAttr: true }) middle: string;
+  @Prop({ mutable: true, reflectToAttr: true }) last: string;
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
+  @State() editing: boolean = false;
 
-  /**
-   * The last name
-   */
-  @Prop() last: string;
+  firstEl: HTMLInputElement;
 
   private getText(): string {
     return format(this.first, this.middle, this.last);
   }
 
+  beginEditing() {
+    this.editing = true;
+    this.firstEl.focus();
+  }
+
+  renderName() {
+    return <div onClick={() => this.beginEditing()} class="displaying">Hello, World! I'm {this.getText()}</div>;
+  }
+
+  renderInput() {
+    return <div class="editing">
+      <label>
+        First:
+        <input 
+          type="text" 
+          value={this.first} 
+          ref={elm => this.firstEl = elm}
+          onInput={(event) => this.first = (event.target as HTMLInputElement).value} />
+      </label>
+      <label>
+        Middle:
+        <input 
+          type="text" 
+          value={this.middle} 
+          onInput={(event) => this.middle = (event.target as HTMLInputElement).value} />
+      </label>
+      <label>
+        Last:
+        <input 
+          type="text" 
+          value={this.last} 
+          onInput={(event) => this.last = (event.target as HTMLInputElement).value} />
+      </label>
+      <button onClick={() => this.editing = false}>Done</button>
+    </div>
+  }
+
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return this.editing ? this.renderInput() : this.renderName();
   }
 }
