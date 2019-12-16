9de2099ae965bd45807d54125f00d8bba799deae
Alan Chambers <ajmchambers@users.noreply.github.com>
2019-12-16 11:44:32 +0000

    Update stencil.config.ts

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,10 +1,15 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'mycomponent',
-  outputTargets:[
-    { type: 'dist' },
-    { type: 'docs' },
+  namespace: 'stencil-component',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
     {
       type: 'www',
       serviceWorker: null // disable service workers
